2aa563f8736e76742c28113bc3291d06d2fdab22
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-09-23 15:02:29 +0900

    Inputとボタンを用意

--- oauth-checker-frontend/src/App.js
+++ oauth-checker-frontend/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Input, Button } from 'react-rainbow-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const inputContainerStyles = {
+  width: '50%',
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="rainbow-p-around_x-large">
+            <div className="rainbow-flex rainbow-p-bottom_medium">
+              <div className="rainbow-p-horizontal_small" style={{width: '100%'}}>
+                <Input
+                  label="Input URLScheme"
+                  placeholder="URL"
+                  type="text" />
+              </div>
+            </div>
+          </div>
+          <div className="rainbow-p-vertical_large rainbow-align-content_center rainbow-flex_wrap">
+            <div className="rainbow-m-horizontal_medium">
+              <Button
+                label="Execute"
+                onClick={() => alert('clicked!')}
+                variant="brand" />
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
