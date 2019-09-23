b708e914f3d097e9162f8b17aecad2b900844b8b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-09-23 13:31:59 +0900

    change layout.

--- oauth-checker/src/App.tsx
+++ oauth-checker/src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
+const inputContainerStyles = {
+  width: '50%',
+};
+
+class App extends React.Component {
+  render():
+  | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+  | string
+  | number
+  | {}
+  | React.ReactNodeArray
+  | React.ReactPortal
+  | boolean
+  | null
+  | undefined {
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,6 +33,7 @@ const App: React.FC = () => {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
